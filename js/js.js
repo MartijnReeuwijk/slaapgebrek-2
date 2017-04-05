9d8840f9f5bb69b680df7b479b5f7c4f49d2257e
Martijn <m.reeuwijk@live.nl>
2017-04-05 15:07:58 +0200

    Sick anime bruh

--- js/js.js
+++ js/js.js
@@ -2,16 +2,46 @@ console.log("Hi");
 
 // magic
 $(function() {
-  $('a[href*="#"]:not([href="#"])').click(function() {
-    if (location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'') && location.hostname == this.hostname) {
-      var target = $(this.hash);
-      target = target.length ? target : $('[name=' + this.hash.slice(1) +']');
-      if (target.length) {
-        $('html, body').animate({
-          scrollTop: target.offset().top
-        }, 1000);
-        return false;
-      }
-    }
-  });
+    $('a[href*="#"]:not([href="#"])').click(function() {
+        if (location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'') && location.hostname == this.hostname) {
+            var target = $(this.hash);
+            target = target.length ? target : $('[name=' + this.hash.slice(1) +']');
+            if (target.length) {
+                $('html, body').animate({
+                    scrollTop: target.offset().top
+                }, 1000);
+                return false;
+            }
+        }
+    });
+});
+
+
+$( document ).ready(function() {
+    var ctrlVideo = document.getElementById("videoplay");
+
+    $('.playpause').click(function(){
+
+        if (!$('.playpause').hasClass("active")){
+            ctrlVideo.play();
+            $('.playpause').toggleClass("active");
+            $('.playpause').removeClass("deactive");
+        }
+
+        if (($('.playpause').hasClass("active"))) {
+            $('#videoplay').click(function() {
+                ctrlVideo.pause();
+                $('.playpause').toggleClass("deactive");
+                $('.playpause').removeClass("active");
+
+            })
+
+        }
+
+        else {
+            ctrlVideo.pause();
+            $('.playpause').toggleClass("deactive");
+        }
+    });
+
 });
